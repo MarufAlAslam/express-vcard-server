2f60216ead12bc4953c9540394fe40bd0bf00cc8
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-08-02 15:46:23 +0600

    updated

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = 8000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 8000;
 
-// cors 
+// cors
 app.use(cors());
 
 // body parser
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri =
+  "mongodb+srv://vcard_admin:h3qrb6FGPUAkXn0g@cluster0.t32itgc.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// create a collection for cards
+const cardCollection = client.db("vcard").collection("cards");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("connected to MongoDB!");
+  } finally {
+  }
+}
+
+run();
+
+// get all cards
+app.get("/api/v1/cards", async (req, res) => {
+  const cursor = cardCollection.find({});
+  const cards = await cursor.toArray();
+  res.send(cards);
+});
+
+// create a card
+app.post("/api/v1/cards", async (req, res) => {
+  const card = req.body;
+  const result = await cardCollection.insertOne(card);
+
+  console.log(
+    `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`
+  );
+  res.send(result);
+});
+
+// update a card and add a new field
+app.put("/api/v1/cards/:id", async (req, res) => {
+    const cardId = req.params.id;
+
+    console.log('updating card with id:', cardId);
+
+    const updatedCard = req.body;
+
+    const filter = { _id: new ObjectId(cardId) };
+
+    const options = { upsert: true };
+
+    const updateDoc = {
+        $set: {
+            ...updatedCard,
+        },
+    };
+
+    cardCollection.updateOne(filter, updateDoc, options);
+
+    res.send(updatedCard);
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
